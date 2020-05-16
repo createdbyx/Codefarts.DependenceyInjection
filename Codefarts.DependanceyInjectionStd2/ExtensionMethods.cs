3a77ce4f9c249c51e20ca6847e71fe82db00ac0d
Dean <deanlunz@createdbyx.com>
2020-05-15 18:00:09 -0700

    initial commit

---
+++ Codefarts.DependanceyInjectionStd2/ExtensionMethods.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="ExtensionMethods.cs" company="Codefarts">
+// Copyright (c) Codefarts
+// contact@codefarts.com
+// http://www.codefarts.com
+// </copyright>
+
+namespace Codefarts.DependencyInjection
+{
+    using System;
+
+    public static class ExtensionMethods
+    {
+        public static void Register<T>(this IDependencyInjectionProvider provider, Func<T> callback)
+        {
+            provider.Register(typeof(T), () => callback());
+        }
+
+        public static T Resolve<T>(this IDependencyInjectionProvider provider)
+        {
+            return (T)provider.Resolve(typeof(T));
+        }
+
+        public static void Register<T>(this IDependencyInjectionProvider provider, Type concrete)
+        {
+            provider.Register(typeof(T), concrete);
+        }
+
+        public static void Register<T, TConcrete>(this IDependencyInjectionProvider provider)
+        {
+            provider.Register(typeof(T), typeof(TConcrete));
+        }
+    }
+}
---
+++ Codefarts.DependanceyInjectionStd2/RegistrationHandlerArgs.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="RegistrationHandlerArgs.cs" company="Codefarts">
+// Copyright (c) Codefarts
+// </copyright>
+
+namespace Codefarts.DependencyInjection
+{
+    using System;
+
+    public delegate void RegistrationHandler(object sender, RegistrationHandlerArgs e);
+
+    public class RegistrationHandlerArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegistrationHandlerArgs"/> class.
+        /// </summary>
+        /// <param name="provider">A reference to the Ioc container.</param>
+        public RegistrationHandlerArgs(IDependencyInjectionProvider provider)
+        {
+            this.DependencyInjectionProvider = provider ?? throw new ArgumentNullException(nameof(provider));
+        }
+
+        public IDependencyInjectionProvider DependencyInjectionProvider
+        {
+            get;
+        }
+    }
+}
